a876f509096264fdef74d25b9ba23fa9c2157d7c
Tejal Singare <tejalsingare@outlook.com>
2022-12-09 23:39:01 +0530

    Migrations

--- Vidly/App_Data/ApplicationDbContext.cs
+++ Vidly/App_Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Vidly.Models;
 
 namespace Vidly
 {
@@ -12,5 +13,7 @@ namespace Vidly
         {
                 
         }
+
+        DbSet<Customer> Customers { get; set; }
     }
 }
