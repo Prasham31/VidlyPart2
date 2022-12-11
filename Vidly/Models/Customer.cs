18bbde093fb17ac995f4e58c77294e0498a8b0f6
Tejal Singare <tejalsingare@outlook.com>
2022-12-11 15:26:08 +0530

    Migrations & Data Annotations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,14 @@ namespace Vidly.Models
     {
         public int id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; } //Navigation Property- as it allows to navigate from one type to another
+
+        public byte MembershipTypeid { get; set; }//Foreign key
     }
 }
